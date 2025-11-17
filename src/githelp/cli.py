0c3dadc094e9001d42dd166baaec6edeea64ce75
Jimmy <zhangjimmy73@gmail.com>
2025-11-17 00:04:41 -0500

    fix tips.yml, adjust code, and added numpydoc

--- src/githelp/cli.py
+++ src/githelp/cli.py
@@ -2,13 +2,13 @@ import click
 from .overlays import load_overlay, render_overlay, render_menu
 
 @click.group(
-    help="githelp — terminal-first Git helper.",
+    help="githelp terminal first Git helper.",
     invoke_without_command=True,
     add_help_option=False,
 )
 @click.option("-h", "--help", "show_help", is_flag=True, help="Provides option menu")
 @click.pass_context
-def main(context, show_help):
+def githelp_cli(context, show_help):
     if show_help or context.invoked_subcommand is None:
         click.echo(context.get_help())
         click.echo()
@@ -16,13 +16,15 @@ def main(context, show_help):
         if context.invoked_subcommand is None:
             context.exit(0)
 
-@main.command(name="list", help="List of available githelp tip pages.")
+@githelp_cli.command(name="list", help="List of available githelp tip pages.")
 def list_cmd():
+    '''List all available githelp tip.'''
     click.echo(render_menu())
 
-@main.command(name="run", help="Show githelp overlay tips for a git subcommand.")
+@githelp_cli.command(name="explain", help="Show githelp overlay tips for a git subcommand.")
 @click.argument("cmd")
-def run_cmd(cmd):
+def explain_cmd(cmd):
+    '''Show tips for a specific git subcommand'''
     tips = load_overlay(cmd)
     if tips:
         click.echo(render_overlay(tips))
